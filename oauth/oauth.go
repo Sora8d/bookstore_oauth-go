42b814d440b988509397bf6070d46e5850921c16
Sora8d <miguelpx2012@gmail.com>
2021-10-08 10:17:43 -0300

    fixed url

--- oauth/oauth.go
+++ oauth/oauth.go
@@ -23,7 +23,7 @@ const (
 var oauthRestClient *rest.Client = rest.New()
 
 func init() {
-	oauthRestClient.SetHostURL("http://127.0.0.1:8080")
+	oauthRestClient.SetHostURL("http://127.0.0.1:8081")
 	oauthRestClient.SetTimeout(100 * time.Millisecond)
 }
 
